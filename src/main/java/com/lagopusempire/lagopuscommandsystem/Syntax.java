ebe8d1cbcce828ed09178d65d5f27f3dfc32ace3
Zora <MrZoraman@Gmail.com>
2015-02-21 00:40:05 -0700

    Successfully maps the commands to a tree-like structure

--- src/main/java/com/lagopusempire/lagopuscommandsystem/Syntax.java
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/Syntax.java
@@ -14,21 +14,43 @@ class Syntax
     
     private ICommand command = null;
     
-    public Syntax(String[] path, ICommand command)
+    public Syntax() { }
+    
+    public void addSyntax(String[] path, ICommand command)
     {
-        if(path.length == 0)
+        final String pathElement = path[0];
+        if(!children.containsKey(pathElement))
         {
-            throw new UnsupportedOperationException("Cannot create syntax with an empty path!");
+            children.put(pathElement, new Syntax());
         }
         
         if(path.length > 1)
         {
-            String[] subPath = Arrays.copyOfRange(path, 1, path.length);
-            children.put(path[0], new Syntax(subPath, command));
+            final String[] subPath = Arrays.copyOfRange(path, 1, path.length);
+            children.get(pathElement).addSyntax(subPath, command);
         }
         else
         {
-            this.command = command;
+            children.get(pathElement).command = command;
+        }
+    }
+    
+    public void print(int level)
+    {
+        for(String path : children.keySet())
+        {
+            printPreSpacing(level);
+            Syntax child = children.get(path);
+            System.out.println(path + ": " + (child.command != null));
+            child.print(level + 1);
+        }
+    }
+    
+    private void printPreSpacing(int level)
+    {
+        for(int ii = 0; ii < level; ii++)
+        {
+            System.out.print("    ");
         }
     }
 }
--- src/test/java/com/lagopusempire/lagopuscommandsystem/MainTest.java
+++ src/test/java/com/lagopusempire/lagopuscommandsystem/MainTest.java
@@ -1,9 +1,60 @@
 package com.lagopusempire.lagopuscommandsystem;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 /**
  *
  * @author MrZoraman
  */
 public class MainTest
 {
+    public static void main(String[] args)
+    {
+        Result result = JUnitCore.runClasses(MainTest.class);
+        for(Failure failure : result.getFailures())
+        {
+            System.out.println(failure.toString());
+        }
+    }
+    
+    @Test
+    public void mainTest()
+    {
+        System.out.println("Hello!");
+    }
+    
+    @Test
+    public void syntaxPrintTest()
+    {
+        Syntax root = new Syntax();
+        
+        String[] cmd1 = {"a", "b", "c"};
+        String[] cmd2 = {"a", "b", "d"};
+        String[] cmd3 = {"a", "b", "e"};
+        String[] cmd4 = {"a", "f"};
+        String[] cmd5 = {"a", "g"};
+        String[] cmd6 = {"h"};
+        String[] cmd7 = {"a"};
+        
+        root.addSyntax(cmd1, new TestCommand());
+        root.addSyntax(cmd2, new TestCommand());
+        root.addSyntax(cmd3, new TestCommand());
+        root.addSyntax(cmd4, new TestCommand());
+        root.addSyntax(cmd5, new TestCommand());
+        root.addSyntax(cmd6, new TestCommand());
+        root.addSyntax(cmd7, new TestCommand());
+        
+        root.print(0);
+    }
+    
+    private class TestCommand implements ICommand
+    {
+        @Override
+        public void execute(String[] preArgs, String[] args)
+        {
+        }
+    }
 }
