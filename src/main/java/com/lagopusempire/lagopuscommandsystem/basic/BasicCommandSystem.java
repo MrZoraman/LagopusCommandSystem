681f05542423f622e766eac675e6ea37345e63bb
Zora <MrZoraman@Gmail.com>
2017-07-15 11:11:59 -0700

    Made some defaults that will be used 95% of the time.

---
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/basic/BasicCommandSystem.java
@@ -0,0 +1,11 @@
+package com.lagopusempire.lagopuscommandsystem.basic;
+
+import com.lagopusempire.lagopuscommandsystem.CommandSystem;
+
+/**
+ *
+ * @author Foomf
+ */
+public class BasicCommandSystem extends CommandSystem<IBasicCommand> {
+    
+}
---
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/basic/IBasicCommand.java
@@ -0,0 +1,9 @@
+package com.lagopusempire.lagopuscommandsystem.basic;
+
+/**
+ *
+ * @author Foomf
+ */
+public interface IBasicCommand {
+    void execute(String[] preArgs, String[] args);
+}
