66e4fefb17c2558b5d33ecfc62e32496b6dc5149
Zora <MrZoraman@Gmail.com>
2017-07-15 11:30:03 -0700

    IBasicCommand is a functional interface

--- src/main/java/com/lagopusempire/lagopuscommandsystem/basic/IBasicCommand.java
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/basic/IBasicCommand.java
@@ -4,6 +4,7 @@ package com.lagopusempire.lagopuscommandsystem.basic;
  *
  * @author Foomf
  */
+@FunctionalInterface
 public interface IBasicCommand {
     void execute(String[] preArgs, String[] args);
 }
