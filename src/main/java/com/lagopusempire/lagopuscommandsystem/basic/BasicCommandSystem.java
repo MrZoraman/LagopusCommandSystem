4c412ed218b760333856cfe2a386da03708aaef2
Zora <MrZoraman@Gmail.com>
2017-07-16 22:17:36 -0700

    Documentation added.

--- src/main/java/com/lagopusempire/lagopuscommandsystem/basic/BasicCommandSystem.java
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/basic/BasicCommandSystem.java
@@ -4,10 +4,17 @@ import com.lagopusempire.lagopuscommandsystem.CommandResult;
 import com.lagopusempire.lagopuscommandsystem.CommandSystem;
 
 /**
- *
- * @author Foomf
+ * Basic Command System. This class will probably satisfy 95% of the
+ * use cases for this library.
  */
 public class BasicCommandSystem extends CommandSystem<IBasicCommand> {
+    
+    /**
+     * Runs a command given the input string.
+     * @param input The input string to parse and match to a command,
+     * if a command is found. If no command is found and no unknown
+     * command is set, then nothing happens.
+     */
     public void runCommand(String input) {
         CommandResult<IBasicCommand> cmdResult = getCommand(input);
         cmdResult.command.ifPresent(cmd -> {
--- src/main/java/com/lagopusempire/lagopuscommandsystem/basic/IBasicCommand.java
+++ src/main/java/com/lagopusempire/lagopuscommandsystem/basic/IBasicCommand.java
@@ -1,10 +1,15 @@
 package com.lagopusempire.lagopuscommandsystem.basic;
 
 /**
- *
- * @author Foomf
+ * Basic command. To be used with the BasicCommandSystem.
  */
 @FunctionalInterface
 public interface IBasicCommand {
+    
+    /**
+     * Executes the command-specific code.
+     * @param preArgs The arguments that were filled with wildcards.
+     * @param args The arguments occurring after the matched command.
+     */
     void execute(String[] preArgs, String[] args);
 }
